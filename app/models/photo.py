a70be2058ef4ed26e6cf4884f2df8f19a8bc114b
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2022-05-02 09:20:26 -0700

    got the migration working for photos

--- app/models/photo.py
+++ app/models/photo.py
@@ -1,11 +1,11 @@
 from .db import db
 
-class Photo(db.Models):
+class Photo(db.Model):
     __tablename__ = 'photos'
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     photo_url = db.Column(db.String, nullable=False)
-    caption = db.Column(db.String)
+    caption = db.Column(db.String(255))
 
     user = db.relationship("User", back_populates="photos")
