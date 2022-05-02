f6d80376fdb292c348c1ed840e97798de7b30f2a
andrew bui <bandrewi98@gmail.com>
2022-05-02 14:04:54 -0700

    render images on photo feed

--- react-app/src/components/photos/PhotoFeed.js
+++ react-app/src/components/photos/PhotoFeed.js
@@ -1,25 +1,30 @@
-import {React, useEffect} from "react";
+import { React, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { grabPhotos } from "../../store/photo";
 
 const PhotoFeed = () => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.session.user)
-    const photos = useSelector(state => state.photos)
+    const photos = useSelector(state => Object.values(state.photos))
 
     useEffect(() => {
         dispatch(grabPhotos())
     }, [dispatch])
 
-    let objects = Object.values(photos)
-    console.log(objects)
-    let entries = Object.entries(objects)
-    console.log(entries)
-
     return (
         <div>
-            <h1>This is your photo feed</h1>
-            <h2>{entries.caption}</h2>
+            {photos.length > 0 &&
+                <>
+                    <h1>This is your photo feed</h1>
+                    <ul>
+                        {photos.map(photo => (
+                            <li key={photo.id}>
+                                <img src={photo.photo_url} />
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            }
         </div>
     )
 }
