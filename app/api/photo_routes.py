b289d1ffbc1c7c1840721cc84783fb3349c41e31
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2022-05-02 09:37:43 -0700

    photo route wip(photo form, photo route)

---
+++ app/api/photo_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required, current_user
+from app.models import User, Photo
+
+photo_routes = Blueprint('photos', __name__)
+
+@photo_routes.route('/', methods=["GET", "POST", "PATCH", "DELETE"])
+@login_required
+def photos():
+    #add following photos later
+    if current_user.id:
+        photos = Photo.query.filter(Photo.user_id == current_user.id).all()
+        response = jsonify(photos)
+    return(response)
