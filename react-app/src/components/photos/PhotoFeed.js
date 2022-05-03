7f289f566ffd88e2c5fe031b2a7d94216e8e647e
Philip McCrickard <pcricket10@gmail.com>
2022-05-03 12:17:39 -0400

    started PhotoSRP

--- react-app/src/components/photos/PhotoFeed.js
+++ react-app/src/components/photos/PhotoFeed.js
@@ -3,59 +3,59 @@ import { useDispatch, useSelector } from "react-redux";
 import { editPhoto, grabPhotos } from "../../store/photo";
 
 const PhotoFeed = () => {
-    const dispatch = useDispatch();
-    // const [editClicked, setEditClicked] = useState(false)
-    // const [caption, setCaption] = useState('')
-    const user = useSelector(state => state.session.user)
-    const photos = useSelector(state => Object.values(state.photos))
+	const dispatch = useDispatch();
+	// const [editClicked, setEditClicked] = useState(false)
+	// const [caption, setCaption] = useState('')
+	const user = useSelector(state => state.session.user)
+	const photos = useSelector(state => Object.values(state.photos))
 
-    useEffect(() => {
-        dispatch(grabPhotos())
-    }, [dispatch])
+	useEffect(() => {
+		dispatch(grabPhotos())
+	}, [dispatch])
 
-    // const handleEdit = (e => {
-    //     e.preventDefault()
-    //     setEditClicked(true)
-    // })
+	// const handleEdit = (e => {
+	//     e.preventDefault()
+	//     setEditClicked(true)
+	// })
 
-    // const handleSubmit = (e => {
-    //     e.preventDefault()
-    //     useparams for id on single resource page
-    //     dispatch(editPhoto(id, caption))
-    // })
+	// const handleSubmit = (e => {
+	//     e.preventDefault()
+	//     useparams for id on single resource page
+	//     dispatch(editPhoto(id, caption))
+	// })
 
-    return (
-        <div>
-            {photos.length > 0 &&
-                <>
-                    <h1>This is your photo feed</h1>
-                    <ul>
-                        {photos.map(photo => (
-                            <li key={photo.id}>
-                                <img src={photo.photo_url} />
-                                <p>{photo.caption}</p>
-                                {/* <button onClick={handleEdit}>Edit</button> */}
-                                {/* {editClicked && (
-                                    <form>
-                                        <label>caption</label>
-                                        <input
-                                            type='text'
-                                            value={photo.caption}
-                                            onChange={e => setCaption(e.target.value)}
-                                        ></input>
-                                        <button
-                                            type='submit'
-                                            onSubmit={handleSubmit}
-                                        >Submit Changes</button>
-                                    </form>
-                                )} */}
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            }
-        </div>
-    )
+	return (
+		<div>
+			{photos.length > 0 &&
+				<>
+					<h1>This is your photo feed</h1>
+					<ul>
+						{photos.map(photo => (
+							<li key={photo.id}>
+								<img src={photo.photo_url} />
+								<p>{photo.caption}</p>
+								{/* <button onClick={handleEdit}>Edit</button> */}
+								{/* {editClicked && (
+										<form>
+												<label>caption</label>
+												<input
+														type='text'
+														value={photo.caption}
+														onChange={e => setCaption(e.target.value)}
+												></input>
+												<button
+														type='submit'
+														onSubmit={handleSubmit}
+												>Submit Changes</button>
+										</form>
+								)} */}
+							</li>
+						))}
+					</ul>
+				</>
+			}
+		</div>
+	)
 }
 
 export default PhotoFeed
--- react-app/src/components/photos/PhotoForm.js
+++ react-app/src/components/photos/PhotoForm.js
@@ -3,48 +3,48 @@ import { useDispatch, useSelector } from "react-redux";
 import { uploadPhoto } from "../../store/photo";
 
 const PhotoForm = () => {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-    const [photoUrl, setPhotoUrl] = useState('')
-    const [caption, setCaption] = useState('')
+	const [photoUrl, setPhotoUrl] = useState('')
+	const [caption, setCaption] = useState('')
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-        const newPhoto = {
-            photoUrl,
-            caption
-        }
+		const newPhoto = {
+			photoUrl,
+			caption
+		}
 
-        const response = await dispatch(uploadPhoto(newPhoto))
-    }
+		const response = await dispatch(uploadPhoto(newPhoto))
+	}
 
-    return (
-        <div>
-            <form>
-                <div>
-                    <label>Photo Url</label>
-                    <input
-                        type="text"
-                        value={photoUrl}
-                        onChange={(e) => setPhotoUrl(e.target.value)}
-                    ></input>
-                </div>
-                <div>
-                    <label>caption</label>
-                    <input
-                        type="text"
-                        value={caption}
-                        onChange={(e) => setCaption(e.target.value)}
-                    >
-                    </input>
-                </div>
-                <div>
-                    <button type="submit" onClick={handleSubmit}>Submit</button>
-                </div>
-            </form>
-        </div>
-    )
+	return (
+		<div>
+			<form>
+				<div>
+					<label>Photo Url</label>
+					<input
+						type="text"
+						value={photoUrl}
+						onChange={(e) => setPhotoUrl(e.target.value)}
+					></input>
+				</div>
+				<div>
+					<label>caption</label>
+					<input
+						type="text"
+						value={caption}
+						onChange={(e) => setCaption(e.target.value)}
+					>
+					</input>
+				</div>
+				<div>
+					<button type="submit" onClick={handleSubmit}>Submit</button>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default PhotoForm
