211dd94e1b273186e1d41920610eb0fe5957eee0
andrew bui <bandrewi98@gmail.com>
2022-05-03 11:35:41 -0700

    like route untested

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,3 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, request, current_user
+
+from app.models import db, Like
 
 like_routes = Blueprint('likes', __name__)
+
+@like_routes.route('/', methods=['POST'])
+def post_like():
+    like_id = request.json
+
+    new_like = Like(
+        user_id = current_user.id,
+        like_id = like_id
+    )
+
+    db.session.add(new_like)
+    db.session.commit()
+
+    return new_like.to_dict()
+
+@like_routes.route('/<int:id>', methods=["DELETE"])
+def delete_like(id):
+    like = Like.query.get(id)
+
+    db.session.delete(like)
+    db.session.commit()
+    
+    return {"Message": "Like deleted successfully"}
