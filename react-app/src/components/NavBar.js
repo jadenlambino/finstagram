e57caf72d2e8f79caedd334764a20a0263550ba9
Philip McCrickard <pcricket10@gmail.com>
2022-05-03 19:38:06 -0400

    initial css

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,32 +2,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import "./NavBar.css"
 
 const NavBar = () => {
   return (
     <nav>
       <ul>
-        <li>
+        <li className='nav-link'>
           <NavLink to='/' exact={true} activeClassName='active'>
             Home
           </NavLink>
         </li>
-        <li>
+        <li className='nav-link'>
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
         </li>
-        <li>
+        <li className='nav-link'>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
         </li>
-        <li>
+        <li className='nav-link'>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
         </li>
-        <li>
+        <li className='nav-link'>
           <LogoutButton />
         </li>
       </ul>
