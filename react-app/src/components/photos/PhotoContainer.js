8eeac1c75efd537c3f65233c2d625396c0143241
andrew bui <bandrewi98@gmail.com>
2022-05-04 10:37:26 -0700

    like button frontend functionality

--- react-app/src/components/photos/PhotoContainer.js
+++ react-app/src/components/photos/PhotoContainer.js
@@ -1,14 +1,30 @@
 import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { createLike, removeLike } from "../../store/like"
 
 export default function PhotoContainer({ photo }) {
-
+    const dispatch = useDispatch()
+    const likes = useSelector(state => state.session.likes)
     const [comment, setComment] = useState('')
+
+    const like = likes.find(like => like.photo_id === photo.id)
+
+    const handleLike = (e) => {
+        e.preventDefault()
+        if (like) {
+            dispatch(removeLike(like.id))
+        } else {
+            dispatch(createLike(photo.id))
+        }
+    }
 
     return (
         <>
             <img src={photo.photo_url} />
             <p>{photo.caption}</p>
-            <button>Like</button>
+            <button
+                onClick={handleLike}
+            >Like</button>
             <form>
                 <input
                     type="text"
