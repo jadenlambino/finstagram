fd87fb683944442318652401e21b0d66df53063b
andrew bui <bandrewi98@gmail.com>
2022-05-04 16:19:47 -0700

    follow button full functionality

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { followUser } from '../store/follows';
+import { followUser, removeFollow } from '../store/follows';
 
 function User() {
   const dispatch = useDispatch()
-  const currentUser = useSelector(state => state.session.user)
   const following = useSelector(state => state.session.following)
   const [user, setUser] = useState({});
   const { userId } = useParams();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const followedUser = following.find(user => user.id === +userId)
 
+  const handleFollow = (e) => {
+    e.preventDefault()
 
-    dispatch(followUser(userId))
+    if (followedUser) {
+      dispatch(removeFollow(userId))
+    } else {
+      dispatch(followUser(userId))
+    }
   }
 
   useEffect(() => {
@@ -33,18 +37,26 @@ function User() {
   }
 
   return (
-    <ul>
-      <li>
-        <strong>User Id</strong> {userId}
-      </li>
-      <li>
-        <strong>Username</strong> {user.username}
-      </li>
-      <li>
-        <strong>Email</strong> {user.email}
-      </li>
-      <button onClick={handleSubmit}>Follow</button>
-    </ul>
-  );
+    <>
+      {following &&
+        <ul>
+          <li>
+            <strong>User Id</strong> {userId}
+          </li>
+          <li>
+            <strong>Username</strong> {user.username}
+          </li>
+          <li>
+            <strong>Email</strong> {user.email}
+          </li>
+          {followedUser ? (
+            <button onClick={handleFollow}>Unfollow</button>
+          ) : (
+            <button onClick={handleFollow}>Follow</button>
+          )}
+        </ul>
+      }
+    </>
+  )
 }
 export default User;
