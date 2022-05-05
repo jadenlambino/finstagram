46af751b834351c845fef15ab731e5b457761a02
andrew bui <bandrewi98@gmail.com>
2022-05-05 14:20:16 -0700

    follow reducer comments

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -132,6 +132,12 @@ export default function reducer(state = initialState, action) {
     case GET_FOLLOWS:
       newState = { ...state }
       // console.log('=======================action', action.user.following)
+
+      //ARRAY
+      // newState["following"] = action.user.following
+      // newState["followers"] = action.user.followers
+
+      // NORMALIZED
       newState['following'] = {}
       action.user.following.forEach(user => {
         newState.following[user.id] = user
@@ -147,10 +153,14 @@ export default function reducer(state = initialState, action) {
       // // newState.following.push(action.user.following)
       // newState.following.push(action.user.following)
       // return newState
+
+      //ARRAY
       // return {
       //   ...state,
       //   following: [...state.following, action.user.following]
       // }
+
+      // NORMALIZED
       newState = { ...state }
       newState.following[action.user.following.id] = action.user.following
       return newState
@@ -162,10 +172,14 @@ export default function reducer(state = initialState, action) {
       // return newState
       // console.log('==================ACTIONID', action.id)
       // console.log('==================', state.following.filter(user => user.id !== +action.id))
+
+      //ARRAY
       // return {
       //   ...state,
       //   following: state.following.filter(user => user.id !== +action.id)
       // }
+
+      // NORMALIZED
       newState = { ...state }
       delete newState.following[action.id]
       return newState
