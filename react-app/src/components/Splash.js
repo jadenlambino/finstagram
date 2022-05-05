47c759fa8fd89dacae65c0d7e7301e385e4ef125
Alex Min <alexmin@2424@yahoo.com>
2022-05-05 14:01:04 -0700

    first draft bio added in middle of div

--- react-app/src/components/Splash.js
+++ react-app/src/components/Splash.js
@@ -10,7 +10,8 @@ const Splash = props => {
   return (
     <>
       <div className='splash-div'>
-      <h1 className='splash-title'> Welcome to Finstagram</h1>
+      <h1 className='splash-title'>Finstagram</h1>
+      <h2 className='splash-bio'> Share your vision, join the Finstagram community today. </h2>
       <LoginForm />
       <div className='bottom-signup'>
       Don't have an account?&nbsp;
