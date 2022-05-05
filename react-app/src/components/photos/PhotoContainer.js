13b0ab3e8154d315958b35714bbda2c9b0940772
andrew bui <bandrewi98@gmail.com>
2022-05-05 14:48:21 -0700

    username displays in PhotoContainer and redirects to user of photo

--- react-app/src/components/photos/PhotoContainer.js
+++ react-app/src/components/photos/PhotoContainer.js
@@ -1,12 +1,19 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 import { createLike, removeLike } from "../../store/like"
 import CommentsForm from "../comments/CommentsForm"
 
 export default function PhotoContainer({ photo }) {
     const dispatch = useDispatch()
+    const user = useSelector(state => state.session.user)
+    const following = useSelector(state => state.session.following)
     const likes = useSelector(state => state.session.likes)
     const like = likes.find(like => like.photo_id === photo.id)
+
+    let followedUser
+    if (Object.keys(following)) followedUser = following[photo.user_id]
+    console.log('==========FOLLOWEDUSER', followedUser)
 
     const handleLike = (e) => {
         e.preventDefault()
@@ -16,8 +23,13 @@ export default function PhotoContainer({ photo }) {
             dispatch(createLike(photo.id))
         }
     }
+
     return (
         <>
+            {followedUser ? (<Link to={`/users/${photo.user_id}`}>{followedUser.username}</Link>)
+                : (<Link to={`/users/${photo.user_id}`}>{user.username}</Link>)
+            }
+
             <img src={photo.photo_url} />
             <p>{photo.caption}</p>
             {like ? (
