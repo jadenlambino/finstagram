f9b2f03fa208489b2451cbd7ba50803958173459
andrew bui <bandrewi98@gmail.com>
2022-05-06 08:25:56 -0700

    demo login button

--- react-app/src/components/Splash.js
+++ react-app/src/components/Splash.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import LoginForm from './auth/LoginForm';
 import SignUpForm from './auth/SignUpForm';
 import { NavLink } from 'react-router-dom';
 import "./Splash.css"
+import { login } from '../store/session';
 
 const Splash = props => {
+  const dispatch = useDispatch()
+
+  const handleDemoLogin = (e) => {
+    e.preventDefault()
+
+    dispatch(login('demo@aa.io', 'password'))
+  }
+
   return (
     <>
       <div className='splash-div'>
-      <h1 className='splash-title'>Finstagram</h1>
-      <h2 className='splash-bio'> Share your vision, join the Finstagram community today. </h2>
-      <LoginForm />
-      <div className='bottom-signup'>
-      Don't have an account?&nbsp;
-      <NavLink to='/sign-up' exact={true} className="splash-signup" activeClassName='active'>
-        Sign Up
-      </NavLink>
-      </div>
+        <h1 className='splash-title'>Finstagram</h1>
+        <h2 className='splash-bio'> Share your vision, join the Finstagram community today. </h2>
+        <LoginForm />
+        <button onClick={handleDemoLogin}>Demo Login</button>
+        <div className='bottom-signup'>
+          Don't have an account?&nbsp;
+          <NavLink to='/sign-up' exact={true} className="splash-signup" activeClassName='active'>
+            Sign Up
+          </NavLink>
+        </div>
       </div>
     </>
   )
