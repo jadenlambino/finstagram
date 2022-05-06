324da299a684192e4f00e7d4205458c1ec986fb9
andrew bui <bandrewi98@gmail.com>
2022-05-06 13:49:42 -0700

    fixed login bug again

--- react-app/src/components/photos/PhotoModal/PhotoSRP.js
+++ react-app/src/components/photos/PhotoModal/PhotoSRP.js
@@ -16,7 +16,7 @@ const PhotoSRP = ({ photo }) => {
   const user = useSelector(state => state.session.user)
   const following = useSelector(state => state.session.following)
   const likes = useSelector(state => state.session.likes)
-  const like = likes.find(like => like.photo_id === photo.id)
+  const like = likes?.find(like => like.photo_id === photo.id)
   const history = useHistory()
 
   const handleEdit = (e) => {
@@ -40,84 +40,73 @@ const PhotoSRP = ({ photo }) => {
   const handleLike = (e) => {
     e.preventDefault()
     if (like) {
-        dispatch(removeLike(like.id))
+      dispatch(removeLike(like.id))
     } else {
-        dispatch(createLike(photo.id))
+      dispatch(createLike(photo.id))
     }
-}
+  }
 
   let functionButtons = (
     <div className="button-container">
-          <button onClick={handleEdit}>edit</button>
-          {editClicked && (
-            <form onSubmit={handleSubmit}>
-              <label>caption</label>
-              <input
-                type="text"
-                value={caption}
-                onChange={e => setCaption(e.target.value)}>
-              </input>
-              <button
-                type="submit"
-                >
-                Submit Changes
-              </button>
-            </form>
-          )}
-          <button onClick={handleDelete}>delete</button>
-        </div>
+      <button onClick={handleEdit}>edit</button>
+      {editClicked && (
+        <form onSubmit={handleSubmit}>
+          <label>caption</label>
+          <input
+            type="text"
+            value={caption}
+            onChange={e => setCaption(e.target.value)}>
+          </input>
+          <button
+            type="submit"
+          >
+            Submit Changes
+          </button>
+        </form>
+      )}
+      <button onClick={handleDelete}>delete</button>
+    </div>
   )
 
   const reveal = (e) => {
     buttons ? setButtons(false) : setButtons(true)
   }
 
-  let followedUser
-    if (Object.keys(following)) followedUser = following[photo.user_id]
-  // console.log('render')
   return (
     <div className="modal-container">
       <img src={photo.photo_url} className='image-container'></img>
       <div className="info-container">
         <div className="photo-info">
           <h3>{photo.caption}</h3>
-          {followedUser ? (
-                <div>
-                    <Link to={`/users/${photo.user_id}`}>{followedUser.username}</Link>
-                </div>
-            )
-                : (
-                    <div>
-                        <Link to={`/users/${photo.user_id}`}>{user.username}</Link>
-                    </div>
-                )
-          }
+          <div>
+            <Link to={`/users/${photo.user_id}`}>{photo.username}</Link>
+          </div>
           {photo.user_id === user.id &&
             <div className="button-menu-container">
-              <input type="checkbox" id="menu-toggle" onChange={reveal}/>
+              <input type="checkbox" id="menu-toggle" onChange={reveal} />
               <label htmlFor='menu-toggle' className="hamburger">
-                  <span className="bun bun-top">
-                      <span className="bun-crust bun-crust-top"></span>
-                  </span>
-                  <span className="bun bun-bottom">
-                      <span className="bun-crust bun-crust-bottom"></span>
-                  </span>
+                <span className="bun bun-top">
+                  <span className="bun-crust bun-crust-top"></span>
+                </span>
+                <span className="bun bun-bottom">
+                  <span className="bun-crust bun-crust-bottom"></span>
+                </span>
               </label>
               {buttons && functionButtons}
             </div>
           }
         </div>
         {like ? (
-              <button
-                  onClick={handleLike}
-              >Unlike</button>
-          ) : (
-              <button
-                  onClick={handleLike}
-              >Like</button>
-          )
+          <button
+            onClick={handleLike}
+          >Unlike</button>
+        ) : (
+          <button
+            onClick={handleLike}
+          >Like</button>
+        )
         }
-        <CommentsFeed photo={photo}/>
+        <CommentsFeed photo={photo} />
         <CommentsForm photo={photo} />
       </div>
     </div>
