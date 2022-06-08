b964009abb4fcc2656cdeda74c5dad1bdaa495a5
andrew bui <bandrewi98@gmail.com>
2022-06-08 01:46:05 -0700

    splash phone image functional

--- react-app/src/components/Splash.js
+++ react-app/src/components/Splash.js
@@ -6,16 +6,16 @@ import "./Splash.css"
 const Splash = props => {
   const images = [
     'https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png',
-    'https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png',
+    'https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png',
     'https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png',
-    'https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png'
+    'https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png'
   ]
   const [imgNum, setImgNum] = useState(0)
 
   useEffect(() => {
     const interval = setInterval(() => {
       setImgNum(prevNum => ++prevNum % images.length)
-    }, 3000)
+    }, 5000)
 
     return () => clearInterval(interval)
   }, [])
@@ -23,7 +23,13 @@ const Splash = props => {
   return (
     <div id='splash-container' className='flex-row'>
       <div id='splash-image-container'>
-        <img id='phone-image' src={images[imgNum]} alt='' />
+        {/* <img id='phone-image-1' className='phone-image' src={images[imgNum]} alt='' /> */}
+        {/* <img id='hidden' className='phone-image' src={images[imgNum + 1]} alt='' /> */}
+        {/* <img id={0 === imgNum && 'ease-in'} className='hidden' src='https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png' alt='' /> */}
+        <img id={0 === imgNum ? 'ease-in' : 'ease-out'} className='phone-image hidden' src='https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png' alt='' />
+        <img id={1 === imgNum ? 'ease-in' : 'ease-out'} className='phone-image hidden' src='https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png' alt='' />
+        <img id={2 === imgNum ? 'ease-in' : 'ease-out'} className='phone-image hidden' src='https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png' alt='' />
+        <img id={3 === imgNum ? 'ease-in' : 'ease-out'} className='phone-image hidden' src='https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png' alt='' />
       </div>
       <div id='login-container' className='flex-column'>
         <div className='splash-div'>
